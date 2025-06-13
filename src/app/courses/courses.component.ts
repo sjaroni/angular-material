c994fbf9f2ebe5df9e7c105b9540b5c96cc33615
Stefan Jaroni <sjaroni@outlook.de>
2025-06-13 17:32:28 +0200

    style: add cards to courses.component

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -2,12 +2,14 @@ import { Component, inject, OnInit } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { Course } from '../models/course';
 import { CourseService } from '../services/course.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-courses',
-  imports: [MatTabsModule],
+  imports: [MatTabsModule, MatCardModule, MatButtonModule],
   templateUrl: './courses.component.html',
-  styleUrl: './courses.component.scss'
+  styleUrl: './courses.component.scss',
 })
 export class CoursesComponent implements OnInit {
   allCourses: Course[] = [];
@@ -18,10 +20,11 @@ export class CoursesComponent implements OnInit {
 
   ngOnInit(): void {
     this.allCourses = this.courseService.courses;
-    this.beginnerCourses = this.courseService.courses.filter(x => x.level === 'beginner');
-    this.advancedCourses = this.courseService.courses.filter(x => x.level === 'advanced');
-    
+    this.beginnerCourses = this.courseService.courses.filter(
+      (x) => x.level === 'beginner'
+    );
+    this.advancedCourses = this.courseService.courses.filter(
+      (x) => x.level === 'advanced'
+    );
   }
-
-
 }
