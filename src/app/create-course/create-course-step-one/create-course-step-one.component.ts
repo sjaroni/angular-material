ae1fe4fc65dd6b5ab6d32cd7a300ded3dbb2ab9b
Stefan Jaroni <sjaroni@outlook.de>
2025-06-15 21:59:15 +0200

    feat: add matInput in reactiveform

--- src/app/create-course/create-course-step-one/create-course-step-one.component.ts
+++ src/app/create-course/create-course-step-one/create-course-step-one.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-create-course-step-one',
-  imports: [],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './create-course-step-one.component.html',
-  styleUrl: './create-course-step-one.component.scss'
+  styleUrl: './create-course-step-one.component.scss',
 })
 export class CreateCourseStepOneComponent {
+  private fb: FormBuilder = inject(FormBuilder);
 
+  form = this.fb.group({
+    title: [
+      '',
+      [Validators.required, Validators.minLength(5), Validators.maxLength(100)],
+    ],
+    description: ['', [Validators.required, Validators.maxLength(500)]],
+    category: ['beginner', Validators.required],
+    courseType: ['premium', Validators.required],
+    downloadAllowed: [false, Validators.required],
+  });
+
+  get courseTitle() {
+    return this.form.get('title');
+    // return this.form.controls['title'];
+  }
 }
